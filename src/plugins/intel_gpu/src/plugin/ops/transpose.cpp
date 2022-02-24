c1e820bf2331bde08a05ba3cdd460eeb654d5a60
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2022-02-24 10:22:17 +0300

    [GPU] Code cleanup

--- src/plugins/intel_gpu/src/plugin/ops/transpose.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/transpose.cpp
@@ -59,8 +59,8 @@ static void CreateTransposeOp(Program& p, const std::shared_ptr<ngraph::op::v1::
         return;
     }
 
-    int rank = std::max(4, static_cast<int>(op->get_input_shape(0).size()));
-    if (order.empty()) {
+    int rank = static_cast<int>(op->get_input_partial_shape(0).rank().get_length());
+    if (ie_order.empty()) {
         // if order size is less than 4 - fill the rest with just copy
         for (int o = rank - 1; o >= 0; o--)
             order.push_back((uint16_t)o);
