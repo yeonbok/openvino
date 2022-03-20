b55c562e41431cd581dae81bf65299380add2428
Felix Dohyun Kim <felix.kim@intel.com>
2022-03-21 04:15:25 +0900

    functional test init

--- src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/gather_elements.cpp
+++ src/tests/functional/plugin/gpu/shared_tests_instances/single_layer_tests/gather_elements.cpp
@@ -2,82 +2,38 @@
 // // SPDX-License-Identifier: Apache-2.0
 // //
 
-// #include <vector>
+#include <vector>
 
-// #include "single_layer_tests/gather_nd.hpp"
-// #include "single_layer_tests/gather_elements.hpp"
-// #include "common_test_utils/test_constants.hpp"
+//NOTE:
+//TEST_P와 FixtureClass(GatherElementsLayerTest)는 여기에 shared되어있다.
+//tensor들은 shape만 지정해주면 그에맞춰 superclass에서 random값을 할당하는듯
+//추가적으로 Fusing까지 체크하는것 같다?
+#include "single_layer_tests/gather_elements.hpp"
+#include "common_test_utils/test_constants.hpp"
 
-// using namespace LayerTestsDefinitions;
-// using namespace ngraph::opset5;
-// using namespace ngraph::opset6;
+using namespace LayerTestsDefinitions;
+using namespace ngraph::opset6;
 
-// namespace {
+namespace {
+const std::vector<InferenceEngine::Precision> inputPrecisions = {
+    // InferenceEngine::Precision::FP32,
+    InferenceEngine::Precision::FP16,
+    // InferenceEngine::Precision::I32,
+};
 
-// const std::vector<InferenceEngine::Precision> inputPrecisions = {
-//         InferenceEngine::Precision::FP32,
-//         InferenceEngine::Precision::FP16,
-//         InferenceEngine::Precision::I32,
-// };
+const std::vector<InferenceEngine::Precision> idxPrecisions = {
+    InferenceEngine::Precision::I32,
+    // InferenceEngine::Precision::I64,
+};
 
-// const std::vector<InferenceEngine::Precision> idxPrecisions = {
-//         InferenceEngine::Precision::I32,
-//         InferenceEngine::Precision::I64,
-// };
-
-// // set1
-// const auto gatherNDArgsSubset1 = ::testing::Combine(
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {2, 2}, {2, 3, 4} })),                                // Data shape
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {2, 1}, {2, 1, 1} })),                                // Indices shape
-//     ::testing::ValuesIn(std::vector<int>({ 0, 1 }))             // Batch dims
-// );
-
-// // set2
-// const auto gatherNDArgsSubset2 = ::testing::Combine(
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {15, 12, 20, 15, 2}, {15, 12, 18, 7, 17} })),         // Data shape
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {15, 12, 2}, {15, 12, 5, 9, 1, 3} })),                // Indices shape
-//     ::testing::ValuesIn(std::vector<int>({ 1, 2 }))             // Batch dims
-// );
-
-// // set3
-// const auto gatherNDArgsSubset3 = ::testing::Combine(
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {4, 3, 2, 5, 5, 2}, {4, 3, 2, 5, 7, 2} })),           // Data shape
-//     ::testing::ValuesIn(std::vector<std::vector<size_t>>(
-//         { {4, 3, 2, 5, 1}, {4, 3, 2, 5, 6, 2} })),              // Indices shape
-//     ::testing::ValuesIn(std::vector<int>({ 3, 4 }))             // Batch dims
-// );
-
-
-// // // -------------------------------- V5 --------------------------------
-// // INSTANTIATE_TEST_SUITE_P(smoke_GatherElements5_set1, GatherNDLayerTest,
-// //     ::testing::Combine(
-// //         gatherNDArgsSubset1,
-// //         ::testing::ValuesIn(inputPrecisions),
-// //         ::testing::ValuesIn(idxPrecisions),
-// //         ::testing::Values(CommonTestUtils::DEVICE_GPU),
-// //         ::testing::Values<Config>({})),
-// //     GatherNDLayerTest::getTestCaseName);
-// // INSTANTIATE_TEST_SUITE_P(smoke_GatherElements5_set2, GatherNDLayerTest,
-// //     ::testing::Combine(
-// //         gatherNDArgsSubset2,
-// //         ::testing::ValuesIn(inputPrecisions),
-// //         ::testing::ValuesIn(idxPrecisions),
-// //         ::testing::Values(CommonTestUtils::DEVICE_GPU),
-// //         ::testing::Values<Config>({})),
-// //     GatherNDLayerTest::getTestCaseName);
-
-// // INSTANTIATE_TEST_SUITE_P(smoke_GatherElements5_set3, GatherNDLayerTest,
-// //     ::testing::Combine(
-// //         gatherNDArgsSubset3,
-// //         ::testing::ValuesIn(inputPrecisions),
-// //         ::testing::ValuesIn(idxPrecisions),
-// //         ::testing::Values(CommonTestUtils::DEVICE_GPU),
-// //         ::testing::Values<Config>({})),
-// //     GatherNDLayerTest::getTestCaseName);
-
-// }  // namespace
+// -------------------------------- V6 --------------------------------
+INSTANTIATE_TEST_SUITE_P(smoke_GatherElements6_set1, GatherElementsLayerTest,
+    ::testing::Combine(
+        ::testing::Values(std::vector<size_t>{4, 2, 3, 1}, std::vector<size_t>{4, 2, 3, 5}),//두 shape중 하나
+        ::testing::Values(std::vector<size_t>{4, 2, 3, 1}, std::vector<size_t>{4, 2, 3, 5}),//두 shape중 하나
+        ::testing::Values(3),//axis=3
+        ::testing::ValuesIn(inputPrecisions),
+        ::testing::ValuesIn(idxPrecisions),
+        ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+    GatherElementsLayerTest::getTestCaseName);
+}  // namespace
