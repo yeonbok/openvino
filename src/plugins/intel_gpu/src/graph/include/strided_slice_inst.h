f032aeba4853294adbbbda097aec38b0b91c5122
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2022-03-30 09:50:54 +0300

    [GPU] Fixes to enable dynamic bert

--- src/plugins/intel_gpu/src/graph/include/strided_slice_inst.h
+++ src/plugins/intel_gpu/src/graph/include/strided_slice_inst.h
@@ -69,6 +69,8 @@ public:
     using parent::parent;
 
     program_node& input(size_t index = 0) const { return get_dependency(index); }
+
+    std::vector<memory::ptr> const_mem = {};
 };
 
 using strided_slice_node = typed_program_node<strided_slice>;
@@ -84,6 +86,8 @@ public:
     static std::string to_string(strided_slice_node const& node);
 
     typed_primitive_inst(network& network, strided_slice_node const& desc);
+
+    void update_shape() override;
 };
 
 using strided_slice_inst = typed_primitive_inst<strided_slice>;
