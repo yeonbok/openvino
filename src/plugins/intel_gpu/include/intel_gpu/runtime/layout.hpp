87a43ed35f8e1097d09ddfe02290984b88dbb2ec
Andrew Park <andrew.park@intel.com>
2022-06-02 23:33:08 +0900

    Revert "Fix PartialShape comparison related issues on TensorIterator/LSTMSequenceTest"
    This reverts commit 664175c19211e1ab4e0e297e8784a33d6cc32750.
    
    Signed-off-by: Andrew Park <andrew.park@intel.com>

--- src/plugins/intel_gpu/include/intel_gpu/runtime/layout.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/runtime/layout.hpp
@@ -350,7 +350,15 @@ struct layout {
     }
 
     friend bool operator==(const layout& lhs, const layout& rhs) {
-        return lhs.data_type == rhs.data_type && lhs.format == rhs.format && lhs.size == rhs.size && lhs.data_padding == rhs.data_padding;
+        auto get_pshape = [&](const layout& l){
+            if (l.format != cldnn::format::any && l.size.size() < l.format.dimension()) {
+                auto dims = l.get_dims();
+                return ov::PartialShape(ov::Shape(dims.begin(), dims.end()));
+            }
+            return l.size;
+        };
+        auto check_pshape = (lhs.is_dynamic() || rhs.is_dynamic()) ? (lhs.size == rhs.size) : (get_pshape(lhs) == get_pshape(rhs));
+        return lhs.data_type == rhs.data_type && lhs.format == rhs.format && check_pshape && lhs.data_padding == rhs.data_padding;
     }
 
     friend bool operator!=(const layout& lhs, const layout& rhs) {
@@ -427,8 +435,6 @@ struct layout {
     std::vector<size_t> get_dims_order() const;
 
     layout convert_to_weights_layout(bool is_grouped) const;
-
-    layout get_layout_with_dims() const;
 
     std::string to_string() const;
 
--- src/plugins/intel_gpu/src/graph/input_layout.cpp
+++ src/plugins/intel_gpu/src/graph/input_layout.cpp
@@ -59,13 +59,7 @@ void input_layout_inst::set_data(memory::ptr mem) {
 
     _has_valid_input = true;
     _output_changed = true;
-    auto check_shape_changed = [&]() {
-        if (ol.is_dynamic())
-            return mem->get_layout() != ol;
-        else
-            return mem->get_layout().get_layout_with_dims() != ol.get_layout_with_dims();
-    };
-    _shape_changed = check_shape_changed();
+    _shape_changed = mem->get_layout() != ol;
 
     if (_shape_changed) {
         node.invalidate_users();
