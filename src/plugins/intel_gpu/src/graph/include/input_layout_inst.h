c8c38258f0d41b0c310680f2a91296f4136ae3e9
Taylor Yeonbok Lee <taylor.lee@intel.com>
2022-08-23 14:02:08 +0900

    accuracy test for static shape passes accuracy test ofr dynamic shape finishes with poor result

--- src/plugins/intel_gpu/src/graph/include/input_layout_inst.h
+++ src/plugins/intel_gpu/src/graph/include/input_layout_inst.h
@@ -34,6 +34,7 @@ public:
     static std::string to_string(input_layout_node const& node);
 
     void update_shape() override;
+public:
     typed_primitive_inst(network& network, input_layout_node const& node);
 
     void set_data(memory::ptr mem);
