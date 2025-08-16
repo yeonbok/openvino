15adac5710fc1be47122184cf7a14e10050cb80f
Taylor Yeonbok Lee <taylor.lee@intel.com>
2025-08-16 16:56:38 +0900

    sdpa with sink

--- src/plugins/intel_gpu/src/plugin/ops/scaled_dot_product_attention.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/scaled_dot_product_attention.cpp
@@ -77,7 +77,7 @@ static void GetNewOrder(ProgramBuilder&p, const std::shared_ptr<ov::op::internal
 
 static void CreateScaledDotProductAttentionOp(ProgramBuilder& p, const std::shared_ptr<ov::op::v13::ScaledDotProductAttention>& op) {
     // if transpose fusion is disabled, this is used
-    validate_inputs_count(op, {3, 4, 5});
+    validate_inputs_count(op, {3, 4, 5, 6});
     auto inputs = p.GetInputInfo(op);
     auto layerName = layer_type_name_ID(op);
 
